51c61d94dae2fd241f2d46a34d8ec770ccc84f77
qiuzhiwei <380013245@qq.com>
2018-01-21 17:18:29 +0800

    sdf

--- index.php
+++
@@ -1,9 +0,0 @@
-<!--加载公共头部-->
-<?php  include 'public/inc/header_inc.php'; ?>
-
-<!--加载当前页面模板-->
-<?php  auto_load_tpl($tplName);  ?>
-
-<!-- 加载公共底部-->
-<?php include 'public/inc/footer_inc.php' ?>
-
